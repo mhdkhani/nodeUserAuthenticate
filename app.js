9acf9d0c5fe7bf7efc3197d866889cb1bd1ba488
root <root@localhost.localdomain>
2021-08-20 13:30:29 +0430

    add css bootstrap

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path')
 const app = express();
 const mongoose = require('mongoose');
 const expressEjsLayout = require('express-ejs-layouts')
 const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require("passport");
+
+
 //passport config:
 require('./config/passport')(passport)
+
+
 //mongoose
 mongoose.connect('mongodb://localhost/tests',{useNewUrlParser: true, useUnifiedTopology : true})
 .then(() => console.log('connected,,'))
 .catch((err)=> console.log(err));
+
+
+
 //EJS
 app.set('view engine','ejs');
 app.use(expressEjsLayout);
 
+
+
+//bootstrap
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
 //BodyParser
 app.use(express.urlencoded({extended : false}));
@@ -44,4 +58,4 @@ next();
 app.use('/',require('./routes/index'));
 app.use('/users',require('./routes/users'));
 
-app.listen(3038); 
+app.listen(3039); 
