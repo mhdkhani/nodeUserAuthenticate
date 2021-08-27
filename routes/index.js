18f30d8764faa222b8cfbde5e7e84056abb0d940
root <root@localhost.localdomain>
2021-08-27 20:11:53 +0430

    changes on routes users

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const router  = express.Router();
-const {ensureAuthenticated} = require("../config/auth.js")
+
 
 
 //login page
 router.get('/', (req,res)=>{
     res.render('welcome' , { title: 'My Site'} );
 })
-//register page
-router.get('/register', (req,res)=>{
-    res.render('register');
-})
 
 
-router.get('/dashboard',ensureAuthenticated,(req,res)=>{
-    res.render('dashboard',{
-        user: req.user
-    });
-})
 
 
 
