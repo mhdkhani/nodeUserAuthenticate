39561a532ed6bd40390d28b1d42d2ff35ea36447
root <root@localhost.localdomain>
2021-09-02 20:00:08 +0430

    remove some files

--- controller/admin/indexController.js
+++
@@ -1,8 +0,0 @@
-module.exports.adminLoginPage = (req,res)=>{
-    res.render('admin/dashboard' , 
-    	{
-    		title: 'geeeeeeeee',
-    		layout: 'admin/admin_layout'
-    	} 
-    );
-}
--- routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const adminController = require('../controller/admin/indexController');
-
-//login page
-router.get('/index',adminController.adminLoginPage)
-
-
-module.exports  = router;
